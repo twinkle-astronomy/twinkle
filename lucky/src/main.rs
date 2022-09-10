2bec214a44a06188b14e34e88092a3d2839552f0
Chris Constantine <cconstan@gmail.com>
2022-09-10 12:39:44 -0700

    better error handling and prompts

--- lucky/src/main.rs
+++ lucky/src/main.rs
@@ -5,6 +5,8 @@ use std::fs;
 use std::num::ParseFloatError;
 use std::path::{Path, PathBuf};
 
+use dialoguer::{theme::ColorfulTheme, Input};
+use std::io::Write;
 use std::{thread, time};
 
 #[derive(Debug)]
@@ -28,7 +30,7 @@ impl From<std::io::Error> for FilterError {
 
 struct FileFilter {
     re: Regex,
-    dst: String,
+    dst: PathBuf,
     fwhm_threshold: f32,
 }
 
@@ -53,7 +55,7 @@ impl FileFilter {
             fs::remove_file(path)?;
         } else {
             let old_path = path.to_owned();
-            let new_path = Path::new(&self.dst).join(path.file_name().unwrap());
+            let new_path = self.dst.join(path.file_name().unwrap());
             fs::rename(old_path, new_path)?;
         }
         Ok(())
@@ -61,23 +63,73 @@ impl FileFilter {
 }
 
 fn main() {
-    let mut files_to_ignore = HashSet::new();
+    println!(
+        r"This script will automatically delete image files if they exceed a certain full width half maximum"
+    );
+    println!(r"It runs as long as nina is open, and stops monitoring once NINA is closed");
+    println!(
+        r"In NINA under options, find image file pattern.  Add '\$$FWHM$$pixels' at the start of the filename"
+    );
+    println!(
+        r"If the filename does not include 'pixels', the program will throw an error about converting string to float"
+    );
+    println!(
+        r"For example, $$TARGETNAME$$\$$DATEMINUS12$$\$$IMAGETYPE$$\$$FILTER$$\$$EXPOSURETIME$$\$$FWHM$$pixels_$$DATETIME$$_$$FILTER$$_$$EXPOSURETIME$$s_$$FRAMENR$$"
+    );
+    println!(
+        r"This uses Hocus Focus for FWHM calculation, if not using this plug in then $$HFR$$pixels can be used instead based on half-flux radius"
+    );
+    println!(r"This is set up for fits files only");
+    // perW=float(input("Fraction of the width to keep (0 - 1, where 0 crops all, 1 keeps full image), ex: 70%=0.7: "))
+    // perH=float(input("Fraction of the height to keep (0 - 1, where 0 crops all, 1 keeps full image),, ex: 70%=0.7: "))
+    let source_dir: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(r"Enter Filepath for monitoring images (e.g. C:\Astrophotography\M31 )")
+        .interact()
+        .unwrap();
+    let mut glob_pattern = PathBuf::from(&source_dir);
+    glob_pattern.push("*.fit");
+    let mut destination = PathBuf::from(&source_dir);
+    destination.push("keepers");
+
+    println!(r"Next select the FWHM or half-flux radius over which images will be deleted");
+    println!(r"Note that this is not the FWHM in arc seconds, but in pixels");
+    println!(
+        r"To convert, take the intended maximum FWHM in arc seconds, and multiply by the focal length (mm) and divide by the pixel size (um) times 206.25"
+    );
+    println!(
+        r"e.g. 3.0 arcsecs threshold*1200mm/3.75um/206.25 yields a threshold FWHM in pixels of 4.8 pixels"
+    );
+    println!(
+        r"HFR is correlated to FWHM but doesn't have a direct conversion.  To use assess the variability of values and select an appropriate threshold"
+    );
+
+    let fwhm_threshold: f32 = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(
+            r"Rejection threshold (FWHM in pixels, or HFR in pixels) above which to delete files",
+        )
+        .interact_text()
+        .unwrap();
 
     let filter = FileFilter {
         re: Regex::new(r"([\d\.]+)pixels").unwrap(),
-        fwhm_threshold: 3.0,
-        dst: "test/keeps".to_string(),
+        fwhm_threshold: fwhm_threshold,
+        dst: destination,
     };
 
-    fs::create_dir_all("test/keeps").unwrap();
+    let mut files_to_ignore = HashSet::new();
+    fs::create_dir_all(&filter.dst).unwrap();
     loop {
-        for entry in glob("test/*.fit").expect("Invalid glob") {
+        for entry in glob(glob_pattern.as_os_str().to_str().unwrap()).expect("Invalid glob") {
             match entry {
                 Ok(path) => {
                     if !files_to_ignore.contains(&path) {
-                        if let Err(e) = filter.handle_file(&path) {
-                            println!("{}: Error -> {:?}", path.display(), e);
-                            files_to_ignore.insert(path);
+                        match filter.handle_file(&path) {
+                            Err(MisingFwhm) => {
+                                println!("{}: missing fwhm in filename", path.display());
+                                files_to_ignore.insert(path);
+                            }
+                            Err(e) => println!("{}: Error -> {:?}", path.display(), e),
+                            _ => (),
                         }
                     }
                 }
