2ef2bb35f41afaf36d38a99378dec764f6704909
Chris Constantine <cconstan@gmail.com>
2023-06-06 11:18:59 -0700

    Add logging feature to phd2_exporter

--- phd2/src/main.rs
+++ phd2/src/main.rs
@@ -1,38 +1,22 @@
-use std::time::Duration;
-
-use phd2::{
-    serialization::{Event, Settle},
-    Phd2Connection,
-};
+use phd2::{serialization::Event, Phd2Connection};
 
 #[tokio::main]
 async fn main() {
-    let mut phd2: Phd2Connection<_> = tokio::net::TcpStream::connect("astro.local:4400")
+    let mut phd2: Phd2Connection<_> = tokio::net::TcpStream::connect("localhost:4400")
         .await
         .expect("Connecting to phd2")
         .into();
-    phd2.loop_().await.unwrap();
+    let mut pixel_scale = phd2.get_pixel_scale().await.expect("Getting pixel scale.");
 
-    phd2.find_star(None).await.ok();
-
-    phd2.guide(
-        Settle {
-            pixels: 1.0,
-            time: Duration::from_secs(5).into(),
-            timeout: Duration::from_secs(60).into(),
-        },
-        None,
-        None,
-    )
-    .await
-    .unwrap();
-
-    let mut sub = phd2.subscribe().await.unwrap();
+    let mut sub = phd2.subscribe().await.expect("Subscribing to events");
 
     while let Ok(event) = sub.recv().await {
         if let Event::GuideStep(guide) = &event.event {
-            let delta = (guide.dx.powi(2) + guide.dy.powi(2)).sqrt();
-            println!("guide error: {:2.2}", delta);
+            let delta = pixel_scale * (guide.dx.powi(2) + guide.dy.powi(2)).sqrt();
+            println!("guide event: {} arcsec.", delta);
+        }
+        if let Event::ConfigurationChange(_) = &event.event {
+            pixel_scale = phd2.get_pixel_scale().await.expect("Getting pixel scale.");
         }
     }
 }
