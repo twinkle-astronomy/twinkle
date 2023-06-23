e58e1cc0a7afa8a9aaf3de08bea265769f258fe7
Chris Constantine <cconstan@gmail.com>
2023-06-23 10:12:46 -0700

    Fix poc ws server

--- twinkle/src/bin/server.rs
+++ twinkle/src/bin/server.rs
@@ -1,51 +1,39 @@
 extern crate actix_web;
-use std::{env, net::TcpStream};
+use std::{env, net::TcpStream, sync::Arc, collections::HashMap};
 
 use actix::prelude::*;
-use actix_web::{HttpRequest, web, HttpResponse, Error, HttpServer, middleware, App, get};
-use actix_web_actors::ws;
-use futures_util::stream::once;
+use actix_web::{HttpRequest, web::{self, Data}, HttpResponse, Error, HttpServer, middleware, App, get};
+
+use indi::client::{notify::Notify, device::Device};
 use serde::{Deserialize, Serialize};
-use tokio::sync::broadcast::{Receiver, Sender};
+use tokio::{sync::{broadcast::{Receiver, Sender}, mpsc}, task::spawn_local};
 use tokio_stream::{StreamExt, wrappers::{BroadcastStream, errors::BroadcastStreamRecvError}};
 
-#[derive(Message)]
-#[rtype(result = "()")]
-struct Ping;
+/// Handshake and start WebSocket handler with heartbeats.
+async fn chat_ws(
+    req: HttpRequest,
+    stream: web::Payload,
+    devices: web::Data<Arc<Notify<HashMap<String, Arc<Notify<Device>>>>>>,
+) -> Result<HttpResponse, Error> {
+    let (res, mut session, _msg_stream) = actix_ws::handle(&req, stream)?;
 
-struct MyWs {
-    recv: Receiver<Vec<String>>
-}
+    let mut devices = devices.subscribe().unwrap();
 
-impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        let stream = BroadcastStream::new(self.recv);
-        Self::add_stream(stream, ctx);
-
-    }
-}
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        println!("handle!");
-    }
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<Vec<String>, BroadcastStreamRecvError>> for MyWs {
-    fn handle(&mut self, msg: Result<Vec<String>, BroadcastStreamRecvError>, ctx: &mut Self::Context) {
-        println!("handle!");
-    }
-}
-
-async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let sender = req.app_data::<Sender<Vec<String>>>().unwrap();
-    let resp = ws::start(MyWs {recv: sender.subscribe() }, &req, stream);
+    // spawn websocket handler (and don't await it) so that the response is returned immediately
+    tokio::spawn(async move {
+        while let Some(Ok(devices)) = devices.next().await {
+            let device_names: Vec<String> = devices.keys().map(|x| x.into() ).collect();
+            if let Err(e) = session.text(format!("{:?}", device_names)).await {
+                dbg!(e);
+                break
+            }
+        }
     
-    println!("{:?}", resp);
-    resp
+        // attempt to close connection gracefully
+        let _ = session.close(None).await;
+    });
+
+    Ok(res)
 }
 
 #[actix_rt::main]
@@ -57,93 +45,17 @@ async fn main() -> std::io::Result<()> {
     let client = indi::client::new(connection, None, None).unwrap();
 
     let devices = client.get_devices();
-    let mut sub = devices.subscribe().unwrap();
-
-    let (tx, _rx) = tokio::sync::broadcast::channel::<Vec<String>>(1024);
-
-    let tx_c = tx.clone();
-    tokio::spawn(async move {
-        while let Some(Ok(update)) = sub.next().await  {
-            let device_names: Vec<String> = update.keys().map(|x| x.into() ).collect();
-
-            tx_c.send(device_names).unwrap();
-            // {"type": "DeviceNames", value: ["device 1", "device 2"]}
-        }
-    });
-
 
     env_logger::init();
 
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .app_data(tx.clone())
+            .app_data(Data::new(devices.clone()))
             // Register HTTP request handlers
-            .route("/ws/", web::get().to(index))
-            .service(list_tweet)
-            .service(get_tweet)
+            .service(web::resource("/ws").route(web::get().to(chat_ws)))
     })
     .bind("0.0.0.0:9090")?
     .run()
     .await
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Response<T> {
-    pub results: Vec<T>,
-}
-
-pub type Tweets = Response<Tweet>;
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Tweet {
-    pub id: u32,
-    pub message: String,
-}
-
-impl Tweet {
-    pub fn new(message: impl Into<String>) -> Self {
-        Self {
-            id: rand::random::<u32>(),
-            message: message.into(),
-        }
-    }
-
-    pub fn foo(&self) {}
-}
-
-#[get("/tweets")]
-pub async fn list_tweet() -> HttpResponse {
-    let tweets = Tweets {
-        results: vec![Tweet::new("First tweet!"), Tweet::new("last tweet")],
-    };
-    let resp = HttpResponse::Ok()
-        .content_type("application/json")
-        // .append_header(("access-control-allow-origin",  "*"))
-        .json(tweets);
-
-    dbg!(resp.body());
-    resp
-}
-
-#[get("/tweets/{id}")]
-pub async fn get_tweet(id: web::Path<u32>) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("application/json")
-        // .append_header(("access-control-allow-origin",  "*"))
-        .json(Tweet::new(format!("A tweet with id: {}", id)))
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct TweetRequest {
-    pub message: Option<String>,
-}
-
-impl TweetRequest {
-    pub fn to_tweet(&self) -> Option<Tweet> {
-        match &self.message {
-            Some(message) => Some(Tweet::new(message.to_string())),
-            None => None,
-        }
-    }
 }
